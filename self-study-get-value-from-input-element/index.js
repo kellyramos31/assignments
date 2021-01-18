5187c3a9a91ce30adeb8b8cb3ac43257a314cceb
Kelly Ramos <ramoskel343@gmail.com>
2021-01-17 17:43:04 -0700

    Initial Commit

--- self-study-get-value-from-input-element/index.js
+++ self-study-get-value-from-input-element/index.js
@@ -1,9 +1,11 @@
 
 
-document.getElementById("click-here").addEventListener("click", function typedStuff() {
+document.getElementById("click-here").addEventListener("click", typedStuff)
+
+function typedStuff() {
     var userTypedThis = document.getElementById("user-input");
     console.log(userTypedThis.value)
     var onPage = document.getElementById("para-this");
     onPage.textContent = userTypedThis.value;
-})
+}
 
