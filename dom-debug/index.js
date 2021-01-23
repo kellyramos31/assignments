06e6e9cc3f60ebc9b21a0bca0b3ed6a78e82d9a6
Kelly Ramos <ramoskel343@gmail.com>
2021-01-22 17:22:52 -0700

    Debugged

--- dom-debug/index.js
+++ dom-debug/index.js
@@ -1,28 +1,32 @@
-const colors = ["red", "blue", "green"]
+const colors = ["white", "red", "blue", "green"]
 
-document.getElementById("add").addEventListener("onclick", function(e){
+document.getElementById("add").addEventListener("click", function (e) {  //changed "onclick" to "click"
+    e.preventDefault();
     const subItem = createSubItem(e)
     document.getElementById("list").appendChild(subItem)
 })
 
-function createDropDown(){
+function createDropDown() {
     const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
+    for (let i = 0; i < colors.length; i++) {   //added .length to colors
+        const option = document.createElement("option")  //added document. before createElement
         option.innerHTML = colors[i]
         option.value = colors[i]
         dropDown.append(option)
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    dropDown.addEventListener("change", function (e) {  //changed "onchange" to "change"
+        e.preventDefault();
+        e.target.parentNode.style.backgroundColor = e.target.value //Error: "uncaught type error; cannot set background color of undefined" changed .parent to .style
+        //changing .parent to .style makes the dropdown turn color instead of the user input
+        //changed to use both .parentNode and .style
     })
     return dropDown
 }
 
-function createSubItem(e){
+function createSubItem(e) {
     const subItem = document.createElement("div")
     var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
+    subItem.textContent = subItemValue.value  //added .value to subItemValue
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
     subItem.setAttribute("class", "subItem")
