c3e1f11b3d993fe23e335db28a62f171e4499356
Kelly Ramos <ramoskel343@gmail.com>
2021-01-26 18:39:31 -0700

    delete and edit working now

---
+++ moms-shopping-list-start/index.js
@@ -0,0 +1,104 @@
+
+//DONE -- PART 1 -- BE ABLE TO ADD NEW ITEMS TO LIST - A user will be able to add list items to the pre-built ul using the pre-built form
+//New list items should have the same format as the li's that came with the git repo
+//DONE --PART 2 -- A user will be able to delete items using an items delete button
+
+const myForm = document.addItem;
+
+myForm.addEventListener("submit", e => {
+    e.preventDefault();
+    var newInput = myForm.title.value                //grab user input
+    //console.log(newInput)                         //check what returns here--seems OK
+    var listItem = document.createElement("li");    //create new li element
+    var newDiv = document.createElement("div");    //create new div -- this is where user input (name = "title") will go
+    var shopList = document.getElementById("list")  //grab entire ul element
+    newDiv.textContent = newInput;                 //place the new user input into the newDiv
+    //console.log(newDiv.textContent)                 //check what returns here
+    var editButton = document.createElement("button")       //create edit button element
+    editButton.textContent = "edit";                        //add text content "edit" to button
+    var deleteButton = document.createElement("button");     //create delete button element
+    deleteButton.textContent = "X";                         //add text content "X" to button
+    listItem.appendChild(newDiv);                        //append the new div to the li 
+    shopList.appendChild(listItem);                     //append li to ul
+    listItem.appendChild(editButton)                      //append delete button to li element
+    listItem.appendChild(deleteButton)                      //append delete button to li element
+    myForm.title.value = "";                        //clear form after submit/return
+
+    editButton.addEventListener("click", e => {
+        e.preventDefault();
+        editButton.textContent = "save";   //change "edit" text to "save"
+        var editBox = document.createElement("input");//have input box appear with list item in it
+        editBox.type = "text";   //give input box type of text
+        listItem.appendChild(editBox);  //append new input box to the list item.
+        if (editBox.text !== "") {
+            editBox.addEventListener("change", e => {
+                e.preventDefault();
+                editButton.textContent = "edit";
+                newDiv.textContent = editBox.value; //user can edit and "save" input box's value
+                editBox.remove();
+            })
+        }
+    })
+
+
+    deleteButton.addEventListener("click", e => {   //delete list item if X button is clicked
+        listItem.remove();
+    })
+})
+
+
+
+
+/*
+EXTRA CREDIT:
+A user will be able to click the "edit" button and see an input box appear
+When the user clicks the "edit" button, a "save" button replace the "edit" button
+The input box will automatically have the value of the list item
+The user can edit and "save" the input box's value
+On save, the input box will disappear, and the new value will appear
+*/
+
+
+
+//ASSIGNMENT -- INSTRUCTIONS:
+/*
+SETUP:
+Use the following repo to start your project. This code will help you understand any patterns you should use when
+completing this assignment.
+
+Follow the instructions in the README.
+
+The three items already there are to show how the rest should be structured, but should be removed or commented out
+before moving on to part 2.
+
+PARTS:
+Each part is it's own task. It would be beneficial to have someone look at your code before moving from one part to the
+next.
+
+PART 1:
+A user will be able to add list items to the pre-built ul using the pre-built form
+New list items should have the same format as the li's that came with the git repo
+
+PART 2:
+Part 2--A user will be able to delete items using an items delete button
+
+
+EXTRA CREDIT:
+A user will be able to click the "edit" button and see an input box appear
+When the user clicks the "edit" button, a "save" button replace the "edit" button
+The input box will automatically have the value of the list item
+The user can edit and "save" the input box's value
+On save, the input box will disappear, and the new value will appear
+Passing criteria: Demo to an instructor
+
+An instructor or TA will sit with you for a demonstration of this project. They will be assessing your proficiency in
+the following areas:
+
+Difference between web app and web site
+Build very basic web apps (site is functional and based on user interaction)
+Understand JSON
+Passing off this project completes the Web Applications, Level 1 branch of the skills tree.
+
+EXTRA CREDIT:
+Use localStorage or sessionStorage to save the list in case the page is refreshed.
+*/
