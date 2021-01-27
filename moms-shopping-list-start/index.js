1ce90d9394cf376718e09d69dcbc4303537b88ce
Kelly Ramos <ramoskel343@gmail.com>
2021-01-27 12:20:46 -0700

    Plus code for the two extra credits

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,19 +1,32 @@
 
 //DONE -- PART 1 -- BE ABLE TO ADD NEW ITEMS TO LIST - A user will be able to add list items to the pre-built ul using the pre-built form
 //New list items should have the same format as the li's that came with the git repo
-//DONE --PART 2 -- A user will be able to delete items using an items delete button
+
+//DONE -- PART 2 -- A user will be able to delete items using an items delete button
+/*
+DONE -- 1st EXTRA CREDIT:
+A user will be able to click the "edit" button and see an input box appear
+When the user clicks the "edit" button, a "save" button replace the "edit" button
+The input box will automatically have the value of the list item
+The user can edit and "save" the input box's value
+On save, the input box will disappear, and the new value will appear
+*/
+
+/*NOTE:  ADDED CODE FOR THIS, but not sure if correct 2nd EXTRA CREDIT:  
+Use localStorage or sessionStorage to save the list in case the page is refreshed.*/
+
 
 const myForm = document.addItem;
 
 myForm.addEventListener("submit", e => {
     e.preventDefault();
-    var newInput = myForm.title.value                //grab user input
-    //console.log(newInput)                         //check what returns here--seems OK
-    var listItem = document.createElement("li");    //create new li element
-    var newDiv = document.createElement("div");    //create new div -- this is where user input (name = "title") will go
-    var shopList = document.getElementById("list")  //grab entire ul element
-    newDiv.textContent = newInput;                 //place the new user input into the newDiv
-    //console.log(newDiv.textContent)                 //check what returns here
+    var newInput = myForm.title.value                   //grab user input
+    //console.log(newInput)                             //check what returns here--seems OK
+    var listItem = document.createElement("li");          //create new li element
+    var newDiv = document.createElement("div");         //create new div -- this is where user input (name = "title") will go
+    var shopList = document.getElementById("list")         //grab entire ul element
+    newDiv.textContent = newInput;                      //place the new user input into the newDiv
+    //console.log(newDiv.textContent)                   //check what returns here
     var editButton = document.createElement("button")       //create edit button element
     editButton.textContent = "edit";                        //add text content "edit" to button
     var deleteButton = document.createElement("button");     //create delete button element
@@ -22,41 +35,33 @@ myForm.addEventListener("submit", e => {
     shopList.appendChild(listItem);                     //append li to ul
     listItem.appendChild(editButton)                      //append delete button to li element
     listItem.appendChild(deleteButton)                      //append delete button to li element
-    myForm.title.value = "";                        //clear form after submit/return
+    myForm.title.value = "";                            //clear form after submit/return
+    //console.log(shopList);
 
     editButton.addEventListener("click", e => {
         e.preventDefault();
-        editButton.textContent = "save";   //change "edit" text to "save"
-        var editBox = document.createElement("input");//have input box appear with list item in it
-        editBox.type = "text";   //give input box type of text
-        listItem.appendChild(editBox);  //append new input box to the list item.
+        editButton.textContent = "save";                    //change "edit" text to "save"
+        var editBox = document.createElement("input");      //have input box appear with list item in it
+        editBox.type = "text";                              //give input box type of text
+        listItem.appendChild(editBox);                      //append new input box to the list item.
         if (editBox.text !== "") {
             editBox.addEventListener("change", e => {
                 e.preventDefault();
-                editButton.textContent = "edit";
-                newDiv.textContent = editBox.value; //user can edit and "save" input box's value
-                editBox.remove();
+                editButton.textContent = "edit";            //change button text back to "edit"
+                newDiv.textContent = editBox.value;         //user can edit and "save" input box's value
+                editBox.remove();                           //remove input box for list item
             })
         }
     })
 
-
-    deleteButton.addEventListener("click", e => {   //delete list item if X button is clicked
+    deleteButton.addEventListener("click", e => {           //delete list item if X button is clicked
         listItem.remove();
     })
+
+    localStorage.setItem("list", "shopList");
+    localStorage.getItem("list");
+    console.log(list.textContent);
 })
-
-
-
-
-/*
-EXTRA CREDIT:
-A user will be able to click the "edit" button and see an input box appear
-When the user clicks the "edit" button, a "save" button replace the "edit" button
-The input box will automatically have the value of the list item
-The user can edit and "save" the input box's value
-On save, the input box will disappear, and the new value will appear
-*/
 
 
 
