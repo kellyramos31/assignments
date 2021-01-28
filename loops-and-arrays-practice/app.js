11de6b0413bc545d42899cc47726260fa74a84ec
Kelly Ramos <ramoskel343@gmail.com>
2021-01-28 16:23:52 -0700

    with additional loop practice added

--- loops-and-arrays-practice/app.js
+++ loops-and-arrays-practice/app.js
@@ -62,16 +62,61 @@ for (var i = 0; i < lightBulbArray.length; i++) {
     }
 }
 
+//Optional Bonus challenge
+//Imagine you have a button that toggles a light on and off. Loop through the following array of numbers
+//and toggle the button the numbers of times for each number.The array[2, 3, 2] would toggle the button 7 times.
 
-/*Optional Bonus challenge
-Imagine you have a button that toggles a light on and off. Loop through the following array of numbers
-and toggle the button the numbers of times for each number.The array[2, 3, 2] would toggle the button 7 times.
+//The light is off to start with.  Log to the console whether or not the light is on at the end.
 
-The light is off to start with.  Log to the console whether or not the light is on at the end.
+//Sample Arrays:
 
-Sample Arrays:
-
-[2, 5, 435, 4, 3] // "The light is on"
-[1, 1, 1, 1, 3]   // "The light is on"
+//[2, 5, 435, 4, 3] // "The light is on"
+////[1, 1, 1, 1, 3]   // "The light is on"
 [9, 3, 4, 2]      // "The light is off"
+
+
+
+//DONE -- OTHER PRACTICE: #1 -- Loop through the following array, and log to the console "hooray" for every party there is.
+
+var eventsAtWork = ["work", "pretend to work", "party", "work", "meeting", "party", "daily grind", "work", "party"]
+
+for (var i = 0; i < eventsAtWork.length; i++) {
+    if (eventsAtWork[i] === "party") {
+        console.log("Hooray!")
+    }
+}
+
+//OTHER PRACTICE #2:  Loop through the following array, and count how many "trues" there are.
+//NOTE:  not sure why this is not console logging anything??
+/*
+var booleans = [true, true, false, true, false, false, false]
+
+for (var i = 0; i < booleans.length; i++) {
+    if (booleans[i] === "true") {
+        console.count("true");
+    }
+}
 */
+
+//DONE -- OTHER PRACTICE #3: Add an isAdmin property to each of the users in this array.
+
+var users = [
+    {
+        name: "Sophie",
+        age: 12
+    },
+    {
+        name: "Larry",
+        age: 32
+    },
+    {
+        name: "Cathy",
+        age: 40
+    }
+]
+
+for (var i = 0; i < users.length; i++) {
+    users[i].isAdmin = "yes";
+    console.log(users);
+}
+
