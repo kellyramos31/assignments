38e501888cb8d837eb12873e3c6d0f4c287a3c1c
Kelly Ramos <ramoskel343@gmail.com>
2021-02-22 20:03:25 -0700

    completed map exercises

---
+++ array-map-exercises/index.js
@@ -0,0 +1,205 @@
+//Use the built -in .map() method on arrays to solve all of these problems
+
+/*Feel free to copy and paste the function and tests in this assignment.
+
+//DONE -- 1) Make an array of numbers that are doubles of the first array
+*/
+
+numArray = [2, 5, 100]
+/*
+//code for this exercise:
+const doubleIt = numArray.map(function doubleNumbers(arr) {
+    return arr * 2;// your code here
+})
+console.log(doubleIt)
+*/
+
+//code rewritten with ES6 syntax:  
+const doubleIt = numArray.map(arr => arr * 2)
+console.log(doubleIt)
+
+//console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+
+
+//DONE -- 2) Take an array of numbers and make them strings
+//code for this exercise:
+/*
+const makeString = numArray.map(function stringItUp(arr) {
+    return arr.toString();
+})
+console.log(makeString)
+*/
+
+//code rewritten with ES6 syntax:  
+const makeString = numArray.map(arr => arr.toString())
+console.log(makeString)
+
+//function stringItUp(arr) {
+//  // your code here
+//}
+//console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
+
+
+
+//DONE -- 3) Capitalize each of an array of names
+//code for this exercise:
+
+namesArr = ["john", "JACOB", "jinGleHeimer", "schmidt"]
+/*
+const resultUpperCase = namesArr.map(function capitalizeNames(namesArr) {
+    return namesArr[0].toUpperCase() + namesArr.substring(1).toLowerCase();
+})
+console.log(resultUpperCase)
+*/
+
+//code rewritten with ES6 syntax:  
+const resultUpperCase = namesArr.map(namesArr => namesArr[0].toUpperCase() + namesArr.substring(1).toLowerCase())
+console.log(resultUpperCase)
+
+//console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
+
+
+
+//DONE -- 4) Make an array of strings of the names
+
+const namesOnly = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]
+
+//code for this exercise:
+/*
+const result = namesOnly.map(function (namesOnly) {
+    return namesOnly.name
+})
+console.log(result)
+*/
+
+//code rewritten with ES6 syntax:  
+const result = namesOnly.map(namesOnly => namesOnly.name)
+console.log(result)
+
+// ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+
+
+//DONE --5) Make an array of strings of the names saying whether or not they can go to The Matrix
+
+const movieAge = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+];
+
+//code for this exercise:
+/*
+const resultMovie = movieAge.map(function makeStrings(movieAge) {
+    if (movieAge.age > 18) {
+        return movieAge.name.concat(" can go to The Matrix!")
+    } else {
+        return movieAge.name.concat(" is underage!!")
+    }
+})
+console.log(resultMovie)
+*/
+//code rewritten with ES6 syntax:  
+
+const resultMovie = movieAge.map(movieAge => {
+    if (movieAge.age > 18) {
+        return movieAge.name.concat(" can go to The Matrix!")
+    } else {
+        return movieAge.name.concat(" is underage!!")
+    }
+})
+console.log(resultMovie)
+
+
+//function makeStrings(arr) {
+// your code here
+//}
+
+// ["Angelina Jolie can go to The Matrix",
+// "Eric Jones is under age!!",
+// "Paris Hilton is under age!!",
+// "Kayne West is under age!!",
+// "Bob Ziroll can go to The Matrix"]
+
+
+
+//DONE -- 6) Make an array of the names in h1s, and the ages in h2s
+
+const readyToPutInTheDom = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]
+
+/*
+//code for this exercise:
+const domReady = readyToPutInTheDom.map(function readyForDom(readyToPutInTheDom) {
+    return "<h1>" + readyToPutInTheDom.name + "</h1>" + "<h2>" + readyToPutInTheDom.age + "</h2>";
+})
+console.log(domReady)
+*/
+
+//code rewritten with ES6 syntax:  
+const domReady = readyToPutInTheDom.map(readyToPutInTheDom => "<h1>" + readyToPutInTheDom.name + "</h1>" + "<h2>" + readyToPutInTheDom.age + "</h2>")
+console.log(domReady)
+
+// ["<h1>Angelina Jolie</h1><h2>80</h2>",
+// "<h1>Eric Jones</h1><h2>2</h2>",
+// "<h1>Paris Hilton</h1><h2>5</h2>",
+// "<h1>Kayne West</h1><h2>16</h2>",
+// "<h1>Bob Ziroll</h1><h2>100</h2>"]
--- go-speed-clicker/index.js
+++ go-speed-clicker/index.js
@@ -1,12 +1,37 @@
 
 let countClicks = 0;
+let time = 0;
+
+
 
 window.addEventListener("click", () => {
-    countClicks += 1;
-    localStorage.setItem("clicks", countClicks);
-    displayClicks.textContent = countClicks;
-    //console.log(displayClicks);
+    //setTimeout(function () {
+    //clearInterval(intervalId);
+    //localStorage.setItem("clicks", countClicks);
+    //countClicks += 1;
+    //displayClicks.textContent = countClicks;
+    //}, 10000)
+    setTimeout(function () {
+        countClicks += 1;
+        localStorage.setItem("clicks", countClicks);
+        displayClicks.textContent = countClicks
+    })
+    var intervalId = setInterval(function () {
+        const displayTime = document.getElementById("show-timer");
+        displayTime.textContent = time
+        time += 1;
+        //displayClicks.textContent = countClicks;
+        clearInterval(intervalId); alert('stop');
+    }, 10000)
+
+    //setTimeout(() => {
+
 })
+
+//let timerId = setInterval(() => alert('tick'), 2000);
+//setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000)
+//console.log(displayTime)
+
 
 const displayClicks = document.getElementById("number-clicks");
 const lastCountClicks = localStorage.getItem("clicks");
@@ -17,7 +42,36 @@ if (lastCountClicks) {
 }
 
 
+//Extra credit: Put a countdown timer(10 - 30 seconds) on the page that stops the user's clicks from counting towards the total after the timer ends.
+//setInterval
+//clearInterval
+//setTimeout
 
-setInterval
-clearInterval
-setTimeout
+//setInterval
+//setInterval(function () {
+//    console.log('hello');
+//}, 1000)
+
+// 1 second
+//'hello!'
+// 2 seconds 
+//'hello!'
+
+//var intervalID = setInterval(function () {
+//    console.log('hello!')
+//}, 1000)
+//The variable you assign to setInterval will store the ID for that interval.You can think about the ID 
+//being like the off switch for the interval or timeout you set.
+
+
+//clearInterval
+//When you need to have an interval or timeout stopped, you call the clearInterval(intervalID) 
+//or clearTimeout(timeoutID) and pass in the ID of the one you need stopped.
+
+//setTimeout
+//setTimeout(arg1, arg2)
+//setTimeout(function(){}, 1000)  1000 = 1 second b/c measures milliseconds
+
+
+
+
