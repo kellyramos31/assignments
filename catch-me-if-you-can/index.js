6356405191e47c1efd574db24fdaf0697eb96134
Kelly Ramos <ramoskel343@gmail.com>
2021-03-01 17:12:00 -0700

    added example

--- catch-me-if-you-can/index.js
+++ catch-me-if-you-can/index.js
@@ -77,4 +77,20 @@ catch (err) {
 
 
 
+//EXAMPLE FROM V SCHOOL ARTICLE:
+var input = "";
 
+try {
+    if (input == "") {
+        throw "Input can not be empty";
+    } else if (input.length > 5) {
+        throw "Input is to big";
+    } else {
+        console.log("You code runs fine!");
+    }
+} catch (err) {
+    console.log(err);
+} finally {
+    console.log("Runs no matter what");
+    console.log("And the input was " + input);
+}
