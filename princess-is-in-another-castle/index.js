de443a9cc263d0552b5cdcaab106640347a73cc1
Kelly Ramos <ramoskel343@gmail.com>
2021-03-11 20:21:23 -0700

    a bit more done

--- princess-is-in-another-castle/index.js
+++ princess-is-in-another-castle/index.js
@@ -1,60 +1,83 @@
 
-let gameActive = true;
-
 class Player {
-    constructor(name, totalCoins, status, hasStar) {
+    constructor(name, totalCoins, status, hasStar, gameActive) {
         this.name = name;
-        this.totalCoins = totalCoins;
-        this.status = status;
+        this.totalCoins = 0;
+        this.status = " ";
         this.hasStar = false;
+        this.gameActive = true;
     }
-    setName(namePicked) {
-        if (namePicked === "Mario") {
-            Player.name = "Mario";
+    setName() {
 
-        } else if (namePicked === "Luigi") {
-            Player.name = "Luigi"
+    }
+    gotHit() {
+        if (this.status === "Powered Up") {
+            this.status === "Big";
+        } else if (this.status === "Big") {
+            this.status === "Small"
+        } else if (this.status === "Small") {
+            this.status === "Dead"
         }
+        print();
+    }
+    gotPowerUp() {
+        if (this.status === "Powered Up") {
+            this.hasStar = true;
+        } else if (this.status === "Small") {
+            this.status === "Big"
+        } else if (this.status === "Big") {
+            this.status === "Powered Up"
+        }
+        print()
+    }
+    addCoin() {
+        this.totalCoins += 1;
+        print()
+    }
+    print() {
+        console.log(this)
+    }
+
+}
+
+
+const intervalId = setInterval(randomNumGen, 1000)
+function stopGame() {
+    if (gameActive = false) {
+        clearInterval(intervalId)
     }
 }
 
-/*
- gotHit() {
-    const statusNumber = Math.floor(Math.random() * 2)   //Random number between 0 & 2 (so can be 0, 1 or 2)
-    if(statusNumber === 0) {
+//THIS GETS CALLED INSIDE SET INTERVAL FUNCTION
+function randomNumGen() {
+    const name = Math.floor(Math.random() * 2)   //Random number between 0 & 1 (so can be 0 or 1)
+    if (name === 0) {
+        const namePicked = new Player("Mario", 0, "Powered Up", false, true);
+        console.log(namePicked)
+        return namePicked;
+
+    } else if (name === 1) {
+        const namePicked = new Player("Luigi", 0, "Powered Up", false, true);
+        console.log(namePicked)
+        return namePicked;
+    }
+
+    const statusNumber = Math.floor(Math.random() * 3)   //Random number between 0 & 2 (so can be 0, 1 or 2)
+    if (statusNumber === 0) {
         gotHit()
     } else if (statusNumber === 1) {
         gotPowerUp()
-    } else if (statusNumber === 2){
+    } else if (statusNumber === 2) {
         addCoin()
+    } else if (Player.status === "Dead") {
+        Player.gameActive = false
+        stopGame()
     }
- }
-
- gotPowerUp() {
-
- }
-
- addCoin() {
-
- }
- print() {
-
- }
 }
 
-*/
+setInterval(randomNumGen, 1000)
 
-const name = Math.floor(Math.random() * 2)   //Random number between 0 & 1 (so can be 0 or 1)
-if (name === 0) {
-    namePicked = "Mario"
-    namePicked = new Player(namePicked, 0, "")
-    setName(namePicked)
-} else if (name === 1) {
-    namePicked = "Luigi"
-    setName(namePicked)
-}
 
-console.log(Player.name)
 
 
 
