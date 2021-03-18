1f1c7eb5cc6f27208b312f67a40428a8dc4ab3b4
Kelly Ramos <ramoskel343@gmail.com>
2021-03-18 14:01:14 -0600

    done

--- pokemon-list/index.js
+++ pokemon-list/index.js
@@ -22,3 +22,14 @@ function showOnPage(arr) {
         document.body.appendChild(h3);
     }
 }
+
+/*
+//function to show all Pokemon resource_uri info on web page -- not sure what other details are really here...
+function showOnPage(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const h3 = document.createElement('h3');
+        h3.textContent = arr[i].resource_uri;
+        document.body.appendChild(h3);
+    }
+}
+*/
