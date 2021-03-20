928cb70ce648bebdc59204c7148878fd11129f37
Kelly Ramos <ramoskel343@gmail.com>
2021-03-19 18:48:34 -0600

    initial commit

---
+++ axios-todo-capstone-project/app.js
@@ -0,0 +1,26 @@
+
+axios.get("https://api.vschool.io/kellyr/todo")         //Axios GET request
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
+
+
+
+/*
+function getTodos() {              //function to read/GET a todo and add it to the DOM
+
+}
+
+function createTodo() {                 //function to create/POST a new todo and add it to the DOM
+
+}
+
+function updateTodo() {               //function to update/PUT a todo that's on the DOM
+
+}
+
+
+function deleteTodo() {               //function to destroy/DELETE a todo from the DOM
+
+}
+*/
