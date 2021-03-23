81769fc31e0ec4d48d03292f68491a265df84300
Kelly Ramos <ramoskel343@gmail.com>
2021-03-23 16:55:02 -0600

    get and post

--- axios-todo-capstone-project/app.js
+++ axios-todo-capstone-project/app.js
@@ -1,4 +1,30 @@
+//Axios GET request for Current To Do List
 
+axios.get("https://api.vschool.io/kellyr/todo")
+    .then(response => {
+        for (let i = 0; i < response.data.length; i++) {
+            const subject = document.createElement("div");
+            const detail = document.createElement("li")
+            const imageInfo = document.createElement("img")
+            subject.textContent = response.data[i].title;
+            detail.textContent = response.data[i].description;
+            imageInfo.src = response.data[i].imgUrl;
+            imageInfo.height = 100;
+            imageInfo.width = 100;
+            const getList = document.getElementById("list");
+            getList.appendChild(subject)
+            getList.appendChild(detail)
+            detail.appendChild(imageInfo)
+        }
+    })
+    .catch(error => console.log(error))
+
+
+//const getMyList = response.data.map(arr => {
+
+
+
+//Axios POST request (ADDS new TODO to database)
 const todoForm = document.todoForm
 
 todoForm.addEventListener("submit", e => {
@@ -9,6 +35,20 @@ todoForm.addEventListener("submit", e => {
         imgUrl: todoForm.imgUrl.value
     }
 
+    const addToDo = document.createElement("div");
+    const addDescrip = document.createElement("li");
+    const addImage = document.createElement("img");
+    addImage.src = "./assets/sydney-rae-geM5lzDj4Iw-unsplash.jpg";
+    addToDo.textContent = todoForm.title.value;
+    addDescrip.textContent = todoForm.description.value;
+    addImage.textContent = todoForm.imgUrl.value;
+    addImage.height = 100;
+    addImage.width = 100;
+    const newForList = document.getElementById("list");
+    newForList.appendChild(addToDo);
+    newForList.appendChild(addDescrip);
+    newForList.appendChild(addImage);
+
     axios.post("https://api.vschool.io/kellyr/todo", newtodo)         //Axios POST request (ADDS new TODO to database)
         .then(response => console.log(response.data))
         .catch(error => console.log(error))
@@ -18,11 +58,6 @@ todoForm.addEventListener("submit", e => {
 
 
 
-//Axios GET request
-/*
-axios.get("https://api.vschool.io/kellyr/todo")
-    .then(response => console.log(response.data))
-    */
 
 
 //function to create/POST a new todo and add it to the DOM
