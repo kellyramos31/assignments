3f40b4d180e12774e703f80e67f983c11eb79376
Kelly Ramos <ramoskel343@gmail.com>
2021-04-07 15:47:12 -0600

    practice again

--- array-sort-exercises/index.js
+++ array-sort-exercises/index.js
@@ -7,6 +7,11 @@
 //code for this exercise:
 
 numArr = [1, 90, 5, 2, 3, 20]
+
+//practice again
+const smallToLarge = numArr.sort((a, b) => a - b)
+console.log(smallToLarge)
+
 /*
 numArr.sort(function leastToGreatest(a, b) {
     return a - b;
@@ -14,13 +19,22 @@ numArr.sort(function leastToGreatest(a, b) {
 console.log(numArr)
 */
 //re-written to shorter ES6 version:
+/*
 numArr.sort((a, b) => a - b)
 console.log(numArr)
+*/
+
 
 //console.log(leastToGreatest()); // [1, 2, 3, 5, 20, 90] 
 
 //DONE --2) Sort an array from largest number to smallest
 //code for this exercise:
+
+//practice again
+const largestToSmallest = numArr.sort((a, b) => b - a)
+console.log(largestToSmallest)
+
+
 /*
 numArr.sort(function greatestToLeast(a, b) {
     return b - a;
@@ -28,14 +42,20 @@ numArr.sort(function greatestToLeast(a, b) {
 console.log(numArr)
 */
 //rewritten in shorter ES6:
+/*
 numArr.sort((a, b) => b - a)
 console.log(numArr)
-
+*/
 //console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
 
 
 //DONE -- 3) Sort an array from shortest string to longest
 wordArr = ["dog", "wolf", "by", "family", "eaten"]
+
+//practice again
+const shortStringToLongString = wordArr.sort((a, b) => a.length - b.length)
+console.log(shortStringToLongString)
+
 
 //code for this exercise:
 /*
@@ -45,18 +65,25 @@ wordArr.sort(function lengthSort(a, b) {
 console.log(wordArr); // ["by", "dog", "wolf", "eaten", "family"]
 */
 //re-written in shorter ES6:
+/*
 wordArr.sort((a, b) => a.length - b.length)
 console.log(wordArr)
-
+*/
 
 //DONE -- 4) Sort an array alphabetically
 //note:  when Googled it, said to just use .sort() without passing in any arguments to alphabetize.
 sameWordArr = ["dog", "wolf", "by", "family", "eaten"]
 
+//practice again 
+const alphabetize = sameWordArr.sort()
+console.log(alphabetize)
+
+
 //code for this exercise:
+/*
 sameWordArr.sort()
 console.log(sameWordArr)
-
+*/
 //console.log(alphabetical()); // ["by", "dog", "eaten", "family", "wolf"]
 
 
@@ -68,6 +95,10 @@ characterArr = [
     { name: "Unlucky Swami", age: 77 }
 ]
 
+//practice again
+const sortByAge = characterArr.sort((a, b) => a.age - b.age)
+console.log(sortByAge)
+
 //code for this exercise:
 /*
 characterArr.sort(function byAge(a, b) {
@@ -76,9 +107,10 @@ characterArr.sort(function byAge(a, b) {
 console.log(characterArr)
 */
 //rewritten in shorter ES6:
+/*
 characterArr.sort((a, b) => a.age - b.age);
 console.log(characterArr)
-
+*/
 
 
 // => [ { name: 'Misunderstood Observer', age: 2 },
