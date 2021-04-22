1b7ec284021c000a9ff1feb490f99a9228a374c4
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-04-22 16:03:03 -0600

    header getting closer

--- blog-props/src/Header.js
+++ blog-props/src/Header.js
@@ -6,9 +6,11 @@ import "./Header.css"
 
 function Header() {
     return (
-        <div>
-            <h1 className="header-text">Clean Blog</h1>
-            <h5>A Blog Theme by Start Bootstrap</h5>
+        <div className="header-container">
+            <div className="color-overlay">
+                <h1 className="header-text">Clean Blog</h1>
+                <p className="subtitle-text">A Blog Theme by Start Bootstrap</p>
+            </div>
         </div>
     )
 }
