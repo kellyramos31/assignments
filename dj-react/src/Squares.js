a799d57660d52701c953662d2de68ccf656bb61f
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-04-27 15:33:22 -0600

    squares to black

--- dj-react/src/Squares.js
+++ dj-react/src/Squares.js
@@ -6,21 +6,26 @@ class Squares extends Component {
         this.state = {
             colors: ["white", "white", "white", "white"]
         }
+        this.changeToBlackOrWhite = this.changeToBlackOrWhite.bind(this)
+    }
+
+    changeToBlackOrWhite() {
+        this.setState({
+            colors: ["black", "black", "black", "black"]
+        })
     }
 
     render() {
         //const squareColors = this.state.colors.map(color =>
         //    <Colors color1={this.state.colors[0]} color2={this.state.colors[1]} color3={this.state.colors[2]} color4={this.state.colors[3]} />)
 
-        //console.log(squareColors)
-
         return (
             <div className="grid-container">
                 <div className="square1" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[0] }}></div>
-                <div className="square2" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[0] }}></div>
-                <div className="square3" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[0] }}></div>
-                <div className="square4" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[0] }}></div>
-                <button className="small-time">Small Time DJ(black and white)</button>
+                <div className="square2" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[1] }}></div>
+                <div className="square3" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[2] }}></div>
+                <div className="square4" style={{ width: "250px", height: "250px", border: "8px solid black", backgroundColor: this.state.colors[3] }}></div>
+                <button onClick={this.changeToBlackOrWhite} className="small-time">Small Time DJ(all black or all white)</button>
             </div>
         )
     }
