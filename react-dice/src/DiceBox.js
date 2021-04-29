2887a83c14bac42c99649e4fecd268ec1eee4d41
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-04-29 16:28:01 -0600

    first task

--- react-dice/src/DiceBox.js
+++ react-dice/src/DiceBox.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react"
 import "./Dice.css"
 
-
 class DiceBox extends Component {
     constructor() {
         super()
 
         this.state = {
-            number1: 0,
-            number2: 0,
-            number3: 0,
-            number4: 0,
-            number5: 0
+            number1: null,
+            number2: null,
+            number3: null,
+            number4: null,
+            number5: null
         }
 
         this.rollTheDice = this.rollTheDice.bind(this)
@@ -20,11 +19,11 @@ class DiceBox extends Component {
 
     rollTheDice() {
         this.setState({
-            number1: 1,
-            number2: 2,
-            number3: 3,
-            number4: 4,
-            number5: 5
+            number1: Math.floor(Math.random() * 6) + 1,
+            number2: Math.floor(Math.random() * 6) + 1,
+            number3: Math.floor(Math.random() * 6) + 1,
+            number4: Math.floor(Math.random() * 6) + 1,
+            number5: Math.floor(Math.random() * 6) + 1
         })
     }
 
@@ -32,12 +31,12 @@ class DiceBox extends Component {
 
         return (
             <div className="squares">
-                <div className="square1" style={{ width: "50px", height: "50px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number1}</div>
-                <div className="square2" style={{ width: "50px", height: "50px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number2}</div>
-                <div className="square3" style={{ width: "50px", height: "50px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number3}</div>
-                <div className="square4" style={{ width: "50px", height: "50px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number4}</div>
-                <div className="square5" style={{ width: "50px", height: "50px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number5}</div>
-                <button onClick={this.rollTheDice}>Roll Dice</button>
+                <div className="square1" style={{ width: "75px", height: "75px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number1}</div>
+                <div className="square2" style={{ width: "75px", height: "75px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number2}</div>
+                <div className="square3" style={{ width: "75px", height: "75px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number3}</div>
+                <div className="square4" style={{ width: "75px", height: "75px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number4}</div>
+                <div className="square5" style={{ width: "75px", height: "75px", border: "8px solid black", backgroundColor: "royalblue" }}>{this.state.number5}</div>
+                <button className="roll" onClick={this.rollTheDice}>Click to Roll Dice</button>
             </div>
         )
     }
