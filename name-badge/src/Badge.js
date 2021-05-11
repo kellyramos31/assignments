9d7ce96855f2df98f35a604b3d7cd7969665fa4a
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-05-11 10:51:44 -0600

    some formatting done on badge

--- name-badge/src/Badge.js
+++ name-badge/src/Badge.js
@@ -1,19 +1,19 @@
 import React from "react"
+import "./Badge.css"
 
 function Badge(props) {
     return (
-        <div>
-            {props.name}
-            {props.email}
-            {props.birthPlace}
-            {props.phone}
-            {props.faveFood}
-            {props.tellAboutSelf}
-
+        <div className="display-badge">
+            <h3 className="badge-title">Badge:</h3>
+            <h3 className="badge-name">Name:  {props.name}</h3>
+            <h3 className="badge-email">email:  {props.email}</h3>
+            <h3 className="badge-birthplace">Place of Birth: {props.birthPlace}</h3>
+            <h3 className="badge-phone">Phone:  {props.phone}</h3>
+            <h3 className="badge-favefood">Favorite Food:  {props.faveFood}</h3>
+            <h3 className="badge-aboutself">{props.tellAboutSelf}</h3>
         </div>
     );
 }
 
 export default Badge
 
-//Q:  should this just be a functional component b/c just display purposes???
