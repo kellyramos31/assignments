e14ec98ddc7d6f5fde8230e83b5341ce43795efb
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-05-11 22:57:33 -0600

    border problem

--- name-badge/src/Badge.js
+++ name-badge/src/Badge.js
@@ -5,7 +5,7 @@ function Badge(props) {
     return (
 
         <div className="badge-display">
-            <h3 className="badge-title">Badge:</h3>
+            <h3 className="badge-title" >BADGE</h3>
             <h3 className="badge-name">Name:  {props.name}</h3>
             <h3 className="badge-email">email:  {props.email}</h3>
             <h3 className="badge-birthplace">Place of Birth: {props.birthPlace}</h3>
