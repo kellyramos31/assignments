6c4b36c3554872baead7f310477723518fcaebb8
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-05-14 11:25:42 -0600

    more dry on badge

--- name-badge/src/Badge.js
+++ name-badge/src/Badge.js
@@ -2,10 +2,14 @@ import React from "react"
 import "./Badge.css"
 
 function Badge(props) {
-    return (
 
-        <div className="badge-display">
-            <h3 className="badge-title" >BADGE</h3>
+    //maybe attempt using randomcolor here to see if can get it to work instead??
+
+    return (
+        < div className="badge-display">
+            {Number(props.index) % 2 === 0
+                ? < h3 className="badge-title" style={{ backgroundColor: "royalblue" }}>BADGE</h3>
+                : < h3 className="badge-title" style={{ backgroundColor: "purple" }}>BADGE</h3>}
             <h3 className="badge-name">Name:  {props.name}</h3>
             <h3 className="badge-email">email:  {props.email}</h3>
             <h3 className="badge-birthplace">Place of Birth: {props.birthPlace}</h3>
@@ -14,8 +18,9 @@ function Badge(props) {
             <h3 className="badge-aboutself"> {props.tellAboutSelf}</h3>
         </div>
 
-    );
+    )
 }
+
 
 export default Badge
 
