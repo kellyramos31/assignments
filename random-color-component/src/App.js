54449e4e5ab5dfd052937657f2dad24f8a3c98fc
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-05-19 13:02:17 -0600

    button added

--- random-color-component/src/App.js
+++ random-color-component/src/App.js
@@ -6,30 +6,54 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      isLoading: false,
       color: ""
     }
-
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  //https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}
 
-  componentDidMount() {
-    console.log("hi")
-
+  handleClick() {
+    console.log("I'm working")
     axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
       .then(response => {
         this.setState({
+          isLoading: true,
           color: "#" + response.data.colors[0].hex
         })
+
       })
       .catch(error => console.log(error))
+    this.setState(prevState => {
+      return {
+        isLoading: !prevState.isLoading
+      }
+
+    })
   }
+
+  //LINK from assignment:  https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}
+
+  // componentDidMount() {
+  //   console.log("hi")
+  //   this.setState({ isLoading: true })
+  //   axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+  //     .then(response => {
+  //       this.setState({
+  //         color: "#" + response.data.colors[0].hex
+  //       })
+  //     })
+  //     .catch(error => console.log(error))
+  // }
 
   render() {
     console.log(this.state.color)
+    let pageDisplay = this.state.isLoading ? "Check out this color!  What do you think?" : null
     return (
       <div>
-        <div className="change-it" style={{ height: "800px", backgroundColor: this.state.color }}></div>
+        <p>{pageDisplay}</p>
+        <div className="change-it" style={{ height: "500px", backgroundColor: this.state.color }}></div>
+        <button onClick={this.handleClick}>Get a New Color</button>
       </div >
     )
   }
