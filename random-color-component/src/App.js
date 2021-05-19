5d85bd5d8f98aa9be423a5b3bad121ee9da86a43
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-05-18 20:51:46 -0600

    basic functionality

--- random-color-component/src/App.js
+++ random-color-component/src/App.js
@@ -1,12 +1,38 @@
-import React from "react"
-import './App.css';
+import React, { Component } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div>
 
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      color: ""
+    }
+
+  }
+
+  componentDidMount() {
+    console.log("hi")
+
+    axios.get("https://www.colr.org/json/color/random?timestamp={new%20Date().getTime()}")
+      .then(response => {
+        this.setState({
+          color: response.data.colors[0].hex
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    console.log(this.state.color)
+    return (
+      <div>
+        <div className="change-it" style={{ backgroundColor: "#" + this.state.color }}> HOWDY</div>
+      </div >
+    )
+  }
 }
 
 export default App;
+
+
