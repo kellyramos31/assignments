3ea8d0db4bcea45502d6a4b8c7c666483ab20d5e
Kelly Ramos <ramos343@gmail.com>
2021-10-05 13:02:58 -0600

    endpoints created

---
+++ todo-backend/routes/todosRouter.js
@@ -0,0 +1,107 @@
+const express = require("express")
+const todosRouter = express.Router()
+const { v4: uuidv4 } = require('uuid'); 
+
+//TODOs- "Fake Data"
+const todos = [
+    {
+    "name": "school",
+    "description": "complete coding assignment",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "home",
+    "description": "clean kitchen",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "dinner",
+    "description": "pick up meal order",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "shopping",
+    "description": "find festive halloween shirt",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "halloween decorations",
+    "description": "find awesome pumpkin for porch",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "car",
+    "description": "schedule maintenance service",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "dentist",
+    "description": "schedule 6 mo cleaning",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+},
+    {
+    "name": "vaccine",
+    "description": "schedule flu vaccine date",
+    "imageUrl": "http://www.myimage....",
+    "completed": false,
+    "_id": uuidv4()
+}
+]
+
+
+//GET ALL Request
+todosRouter.get("/", (req, res)=>{
+    res.send(todos)
+})
+
+//GET ONE Request
+todosRouter.get("/:todoId", (req, res)=>{
+    const todoId = req.params.todoId
+    const foundTodo = todos.find(todo=>todo._id === todoId)
+    res.send(foundTodo)
+    res.send(todos)
+})
+
+
+//POST ONE Request
+todosRouter.post("/", (req, res)=>{
+    const newTodo = req.body
+    newTodo._id = uuidv4()
+    todos.push(newTodo)
+    res.send("newTodo successfully added!")
+})
+
+
+//DELETE ONE Request
+todosRouter.delete("/:todoId", (req, res)=>{
+    const todoId = req.params.todoId
+    const todoIndex = todos.findIndex(todo=>todo._id = todoId)
+    bounties.splice(todoIndex, 1)
+    res.send("Successfully deleted todo!")
+})
+
+
+//PUT Request to UPDATE ONE
+todosRouter.put("/:todoId", (req, res)=>{
+   const todoId = req.params.todoId
+   const todoIndex = todos.findIndex(todo=>todo._id = todoId)
+   const updatedTodo = Object.assign(todos[todoIndex], req.body)
+   res.send(updatedTodo)
+})
+
+
+module.exports = todosRouter
---
+++ todo-backend/server.js
@@ -0,0 +1,19 @@
+// import { v4 as uuidv4 } from 'uuid';
+
+//Server
+const express = require("express")
+const app = express()
+const { v4: uuidv4 } = require('uuid'); 
+
+app.use(express.json())  //Looks for request body and turns it into req.body
+
+//ROUTES
+app.use("/todos", require("./routes/todosRouter.js"))
+
+
+
+
+
+app.listen(7000, ()=>{
+    console.log("The server is running on PORT 7000!")
+})
