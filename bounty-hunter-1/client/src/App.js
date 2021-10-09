9eae02712a5ecd6c15d3193f630bd15bb72f29f6
Kelly Ramos <ramos343@gmail.com>
2021-10-09 11:16:18 -0600

    form added

--- bounty-hunter-1/client/src/App.js
+++ bounty-hunter-1/client/src/App.js
@@ -1,17 +1,27 @@
 import React, {useState, useEffect} from "react"
 import axios from "axios"
 import Bounty from "./Bounty.js"
+import BountyForm from "./BountyForm.js"
 
 function App() {
-  const [bounties, setBounties] = useState([])
-  useEffect(()=>{
+
+ const [bounties, setBounties] = useState([])
+
+ const getBounties = () => {
     axios.get("/bounties")
     .then(res=>setBounties(res.data))
     .catch(err=>console.log(err))
+  }
+
+
+
+  useEffect(()=>{
+    getBounties()
   }, [])
 
   return (
     <div>
+      <BountyForm />
       {bounties.map(bounty=> <Bounty {...bounty} key={bounty.firstName}/>)}
     </div>
   );
---
+++ bounty-hunter-1/client/src/BountyForm.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react"
+
+
+function BountyForm(props) {
+    const initInputs = { firstName: "", lastName: "", living: "", bountyAmount: "", type: ""}
+    const [inputs, setInputs] = useState(initInputs)
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setInputs(prevInputs=>({...prevInputs, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        //post request
+        setInputs(initInputs)
+    }
+
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                name="firstName" 
+                value={inputs.firstName}
+                onChange = {handleChange}
+                placeholder="First Name"
+            />
+             <input 
+                type="text" 
+                name="lastName" 
+                value={inputs.lastName}
+                onChange = {handleChange}
+                placeholder="Last Name"
+            />
+             <input 
+                type="text" 
+                name="living" 
+                value={inputs.living}
+                onChange = {handleChange} 
+                placeholder="Living?(true or false)"
+            />
+             <input 
+                type="number" 
+                name="bountyAmount" 
+                value={inputs.bountyAmount}
+                onChange = {handleChange} 
+                placeholder="Bounty Amount $"
+            />
+             <input 
+                type="text" 
+                name="type" 
+                value={inputs.type} 
+                onChange = {handleChange}
+                placeholder="Sith or Jedi?"
+            />
+
+            <button>Add Bounty</button>
+        </form>
+
+    )
+}
+
+export default BountyForm
