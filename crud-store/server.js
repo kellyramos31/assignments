44233cc485af59604d7b928ed047f00ac5aa973f
Kelly Ramos <ramos343@gmail.com>
2021-10-19 19:37:29 -0600

    beginnings of server added

--- crud-store/server.js
+++ crud-store/server.js
@@ -1,12 +1,34 @@
-const { application } = require("express")
 const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
 
 
 //Middleware
 app.use(express.json())   //looks for a request body and turns it into a req.body
+app.use(morgan("dev"))    //logs requests to the console
 
+//Connect to database
+mongoose.connect("mongodb://localhost:27017/inventoriesdb")
 
 //Routes
+app.use("./inventories", require("./routes/crudRouter.js", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+
+    },
+    ()=>console.log("Connected to the DB")
+))
+
+
+//Error Handler
+app.use((err, req, res, next)=>{
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
 
 
 
