db02d6151f53308affff2f9515d7a16cdea51abc
Kelly Ramos <ramos343@gmail.com>
2021-10-21 11:18:40 -0600

    put working

--- crud-store/routes/crudRouter.js
+++ crud-store/routes/crudRouter.js
@@ -89,24 +89,35 @@ crudRouter.post("/", (req, res, next)=>{
 
 //DELETE ONE Request
 crudRouter.delete("/:crudId", (req, res)=>{
-    Inventory.findOneAndDelete({_id: req.params.crudId}, (err, deletedItem)=>{
+    Inventory.findOneAndDelete({_id: req.params.crudId}, (err, deletedCrud)=>{
         if(err){
             res.status(500)
             return next(err)
         }
     
-        return res.status(200).send(`Successfully deleted ${deletedItem.item} from the DB`)
+        return res.status(200).send(`Successfully deleted ${deletedCrud.item} from the DB`)
 
     })
 })
 
 
 //PUT Request - UPDATE ONE
-crudRouter.put("/:crudId", (req, res)=>{
-   const crudId = req.params.crudId
-   const crudIndex = inventories.findIndex(crud=>crud._id = crudId)
-   const updatedCrud = Object.assign(inventories[crudIndex], req.body)
-   res.send(updatedCrud)
+crudRouter.put("/:crudId", (req, res, next)=>{
+   Inventory.findOneAndUpdate(
+       { _id:  req.params.crudId },    //find this one to update
+       req.body,                     //update the object with this data
+       { new: true },               //send back the updated version please :)
+       (err, updatedCrud)=>{
+           if(err){
+            res.status(500)
+            return next(err)
+        }
+    
+        return res.status(201).send(updatedCrud)
+       }
+       )
+
+
 })
 
 
