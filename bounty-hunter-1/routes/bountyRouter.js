892f2628144d7940ee78a6238aab9f75be10fdff
Kelly Ramos <ramoskel343@gmail.com>
2021-10-23 20:13:33 -0600

    schema built

--- bounty-hunter-1/routes/bountyRouter.js
+++ bounty-hunter-1/routes/bountyRouter.js
@@ -2,112 +2,126 @@ const express = require("express")
 const bountyRouter = express.Router()
 
 
-const { v4: uuidv4 } = require('uuid'); 
+// const { v4: uuidv4 } = require('uuid');
 
 //Bounties - "Fake Data"
-const bounties = [
-    {firstName: "Star",
-     lastName:  "Smith",
-     isAlive: false,
-     bountyAmount:  300,
-     type: "Jedi",
-     _id: uuidv4()
-},
+// const bounties = [
+//     {
+//         firstName: "Star",
+//         lastName: "Smith",
+//         isAlive: false,
+//         bountyAmount: 300,
+//         type: "Jedi",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Rick",
-     lastName:  "Rockandroll",
-     isAlive: true,
-     bountyAmount:  5000,
-     type: "Sith",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Rick",
+//         lastName: "Rockandroll",
+//         isAlive: true,
+//         bountyAmount: 5000,
+//         type: "Sith",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Sling",
-     lastName:  "Bling",
-     isAlive: true,
-     bountyAmount:  450,
-     type: "Jedi",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Sling",
+//         lastName: "Bling",
+//         isAlive: true,
+//         bountyAmount: 450,
+//         type: "Jedi",
+//         _id: uuidv4()
+//     },
 
- 
-    {firstName: "Brave",
-     lastName:  "Heart",
-     isAlive: true,
-     bountyAmount:  1000,
-     type: "Sith",
-     _id: uuidv4()
-    },
 
-    {firstName: "Daisy",
-     lastName:  "Duchess",
-     isAlive: true,
-     bountyAmount:  2500,
-     type: "Jedi",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Brave",
+//         lastName: "Heart",
+//         isAlive: true,
+//         bountyAmount: 1000,
+//         type: "Sith",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Trouble",
-     lastName:  "Smith",
-     isAlive: true,
-     bountyAmount:  5500,
-     type: "Sith",
-     _id: uuidv4()
-    },
-        
-    {firstName: "Chaos",
-     lastName:  "Carter",
-     isAlive: true,
-     bountyAmount:  7500,
-     type: "Jedi",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Daisy",
+//         lastName: "Duchess",
+//         isAlive: true,
+//         bountyAmount: 2500,
+//         type: "Jedi",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Forever",
-     lastName:  "Fighting",
-     isAlive: true,
-     bountyAmount:  2500,
-     type: "Sith",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Trouble",
+//         lastName: "Smith",
+//         isAlive: true,
+//         bountyAmount: 5500,
+//         type: "Sith",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Zap",
-     lastName:  "Zest",
-     isAlive: true,
-     bountyAmount:  300,
-     type: "Jedi",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Chaos",
+//         lastName: "Carter",
+//         isAlive: true,
+//         bountyAmount: 7500,
+//         type: "Jedi",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Dragon",
-     lastName:  "Jones",
-     isAlive: true,
-     bountyAmount:  2500,
-     type: "Sith",
-     _id: uuidv4()
-    },
+//     {
+//         firstName: "Forever",
+//         lastName: "Fighting",
+//         isAlive: true,
+//         bountyAmount: 2500,
+//         type: "Sith",
+//         _id: uuidv4()
+//     },
 
-    {firstName: "Eagle",
-     lastName:  "Foster",
-     isAlive: true,
-     bountyAmount:  200,
-     type: "Sith",
-     _id: uuidv4()
-    }
-]
+//     {
+//         firstName: "Zap",
+//         lastName: "Zest",
+//         isAlive: true,
+//         bountyAmount: 300,
+//         type: "Jedi",
+//         _id: uuidv4()
+//     },
+
+//     {
+//         firstName: "Dragon",
+//         lastName: "Jones",
+//         isAlive: true,
+//         bountyAmount: 2500,
+//         type: "Sith",
+//         _id: uuidv4()
+//     },
+
+//     {
+//         firstName: "Eagle",
+//         lastName: "Foster",
+//         isAlive: true,
+//         bountyAmount: 200,
+//         type: "Sith",
+//         _id: uuidv4()
+//     }
+// ]
 
 
 
 //GET ALL Request
-bountyRouter.get("/", (req, res)=>{
+bountyRouter.get("/", (req, res) => {
     res.send(bounties)
 })
 
 //GET ONE Request
+bountyRouter.get("/:bountyId", (req, res) => {
+    res.send(bounties)
+})
 
 
 //POST ONE Request
-bountyRouter.post("/", (req, res)=>{
+bountyRouter.post("/", (req, res) => {
     const newBounty = req.body
     newBounty._id = uuidv4()
     bounties.push(newBounty)
@@ -116,20 +130,20 @@ bountyRouter.post("/", (req, res)=>{
 
 
 //DELETE ONE Request
-bountyRouter.delete("/:bountyId", (req, res)=>{
+bountyRouter.delete("/:bountyId", (req, res) => {
     const bountyId = req.params.bountyId
-    const bountyIndex = bounties.findIndex(bounty=>bounty._id = bountyId)
+    const bountyIndex = bounties.findIndex(bounty => bounty._id = bountyId)
     bounties.splice(bountyIndex, 1)
     res.send("Successfully deleted bounty!")
 })
 
 
 //PUT Request - UPDATE ONE
-bountyRouter.put("/:bountyId", (req, res)=>{
-   const bountyId = req.params.bountyId
-   const bountyIndex = bounties.findIndex(bounty=>bounty._id = bountyId)
-   const updatedBounty = Object.assign(bounties[bountyIndex], req.body)
-   res.send(updatedBounty)
+bountyRouter.put("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id = bountyId)
+    const updatedBounty = Object.assign(bounties[bountyIndex], req.body)
+    res.send(updatedBounty)
 })
 
 
--- bounty-hunter-1/server.js
+++ bounty-hunter-1/server.js
@@ -4,17 +4,32 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const mongoose = require("mongoose")
 
 
 //Middleware (for every request)
 app.use(express.json())  //Looks for request body and turns it into req.body
 app.use(morgan("dev"))   //Logs requests to the console
 
+//Connect to database
+//note: whatever comes after 27017/ becomes the name of the DB
+//question:  is the deprecation object still necessary?  Not listed on mongoose site same as video anymore
+
+mongoose.connect("mongodb://localhost:27017/bountiesdb",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("Connected to the DB!")
+)
+
 //ROUTES
 app.use("/bounties", require("./routes/bountyRouter.js"))
 
 
 
-app.listen(9000, ()=>{
-    console.log("The server is running on PORT 9000!")
+app.listen(8700, () => {
+    console.log("The server is running on PORT 8700!")
 })
