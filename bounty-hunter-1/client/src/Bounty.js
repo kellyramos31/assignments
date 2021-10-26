2f5f3d1eda7a75b6277e37ae170d4d4f0c667d5f
Kelly Ramos <ramoskel343@gmail.com>
2021-10-25 20:17:07 -0600

    front-back connect better

--- bounty-hunter-1/client/src/Bounty.js
+++ bounty-hunter-1/client/src/Bounty.js
@@ -1,50 +1,51 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import BountyForm from "./BountyForm.js"
 
 
-function Bounty (props) {
+function Bounty(props) {
 
-    console.log(props)
-    const {firstName, lastName, bountyAmount, type, _id, index, editBounty} = props
-    const [editToggle, setEditToggle] = useState(false)
+  console.log(props)
+  const { firstName, lastName, bountyAmount, type, _id, index, editBounty, isAlive } = props
+  const [editToggle, setEditToggle] = useState(false)
 
-    return(
+  return (
     <div className="bounty-card">
-      { !editToggle ?
+      {!editToggle ?
         <>
-            <h1 className="bounty-name">{firstName}{" "}{lastName}</h1>
-            {/* <h3>Alive?:  {alive ? "true" : "false"}</h3> */}
-            <h2 className="bounty-dollars">Bounty: $ {bountyAmount}</h2>
-            <h3>Type:  {type}</h3>
-            <button
-                onClick={()=>props.deleteBounty(_id)}>
+          <h1 className="bounty-name">{firstName}{" "}{lastName}</h1>
+          <h3>Alive?:  {isAlive === true ? "true" : "false"}</h3>
+          <h2 className="bounty-dollars">Bounty: $ {bountyAmount}</h2>
+          <h3>Type:  {type}</h3>
+          <button
+            onClick={() => props.deleteBounty(_id)}>
             Delete
-            </button>
-            <button onClick = {()=>setEditToggle(prevEditToggle=>!prevEditToggle)}>
+          </button>
+          <button onClick={() => setEditToggle(prevEditToggle => !prevEditToggle)}>
             Edit
-            </button>
+          </button>
         </>
-      :
+        :
         <>
-            <BountyForm
-                _id={_id}
-                index={index} 
-                firstName={firstName}
-                lastName={lastName}
-                // alive={alive}
-                editToggle={editToggle}
-                setEditToggle={setEditToggle}
-                bountyAmount={bountyAmount}
-                type={type}
-                submit={editBounty}
-                btnText="Submit Edit"
-            />
-            <button onClick = {()=>setEditToggle(prevEditToggle=>!prevEditToggle)}>
+          <BountyForm
+            _id={_id}
+            index={index}
+            firstName={firstName}
+            lastName={lastName}
+            isAlive={isAlive}
+            editToggle={editToggle}
+            setEditToggle={setEditToggle}
+            bountyAmount={bountyAmount}
+            type={type}
+            submit={editBounty}
+            btnText="Submit Edit"
+          />
+          <button onClick={() => setEditToggle(prevEditToggle => !prevEditToggle)}>
             Close
-            </button>
+          </button>
         </>
       }
     </div>
-    
-)}
+
+  )
+}
 export default Bounty
--- bounty-hunter-1/client/src/BountyForm.js
+++ bounty-hunter-1/client/src/BountyForm.js
@@ -1,67 +1,78 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
 
 
 function BountyForm(props) {
-    const initInputs = { firstName: props.firstName || "", lastName: props.lastName || "", alive: props.alive || "", bountyAmount: props.bountyAmount || "", type: props.type || ""}
+    const initInputs = { firstName: props.firstName || "", lastName: props.lastName || "", alive: props.isAlive || "", bountyAmount: props.bountyAmount || "", type: props.type || "" }
     const [inputs, setInputs] = useState(initInputs)
 
+    const [checked, setChecked] = useState(false)
+
     const handleChange = (e) => {
-        const {name, value} = e.target
-        setInputs(prevInputs=>({...prevInputs, [name]: value}))
+        const { name, value } = e.target
+        setInputs(prevInputs => ({ ...prevInputs, [name]: value }))
+
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        props.submit(inputs, props._id)  
+        props.submit(inputs, props._id)
         setInputs(initInputs)
+        setChecked(!checked)
 
-        if(props.btnText === "Submit Edit"){
-        props.setEditToggle(prevToggle=>!prevToggle) 
+
+        if (props.btnText === "Submit Edit") {
+            props.setEditToggle(prevToggle => !prevToggle)
         }
     }
 
 
 
-    return(
+    return (
         <form className="bounty-form" onSubmit={handleSubmit}>
-           <input 
-                type="text" 
-                name="firstName" 
+            <input
+                type="text"
+                name="firstName"
                 value={inputs.firstName}
-                onChange = {handleChange}
+                onChange={handleChange}
                 placeholder="First Name"
             />
-             <input 
-                type="text" 
-                name="lastName" 
+            <input
+                type="text"
+                name="lastName"
                 value={inputs.lastName}
-                onChange = {handleChange}
+                onChange={handleChange}
                 placeholder="Last Name"
             />
-             <input 
-                type="checkbox" 
-                name="alive" 
-                value={inputs.alive}
-                onChange = {handleChange} 
-                placeholder="Living?(true or false)"
-            />
-             <input 
-                type="number" 
-                name="bountyAmount" 
+            <label>
+                Alive?
+                <input
+                    type="checkbox"
+                    name="checked"
+                    value={checked}
+                    onChange={handleChange}
+                    placeholder="Living?(true or false)"
+                />
+            </label>
+
+
+
+            <input
+                type="number"
+                name="bountyAmount"
                 value={inputs.bountyAmount}
-                onChange = {handleChange} 
+                onChange={handleChange}
                 placeholder="Bounty Amount $"
             />
-             <input 
-                type="text" 
-                name="type" 
-                value={inputs.type} 
-                onChange = {handleChange}
+            <input
+                type="text"
+                name="type"
+                value={inputs.type}
+                onChange={handleChange}
                 placeholder="Sith or Jedi?"
             />
 
-            <button>{props.btnText}</button> 
+            <button>{props.btnText}</button>
 
         </form>
     )
