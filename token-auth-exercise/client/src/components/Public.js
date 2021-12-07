c10c9cd15f9e212420c96b05801771864b49f2f2
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-12-07 12:28:33 -0700

    added part 2 client-side from github

---
+++ token-auth-exercise/client/src/components/Public.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TodoList from './TodoList.js'
+import TodoComp from './TodoComp.js'
+
+export default function Public(){
+  return (
+    <div className="public">
+
+    </div>
+  )
+}
---
+++ token-auth-exercise/client/src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoComp from './TodoComp.js'
+
+export default function TodoList(){
+  return (
+    <div className="todo-list">
+
+    </div>
+  )
+}
