323e28ab0b7df903db12db47964bfbad53df8923
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-12-08 10:59:07 -0700

    signup portion added

--- token-auth-exercise/client/src/context/UserProvider.js
+++ token-auth-exercise/client/src/context/UserProvider.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react"
+import axios from "axios"
 
 
 export const UserContext = React.createContext()
@@ -10,12 +11,21 @@ export default function UserProvider(props){
         todos: []
     }
 
-    const {userState, setUserState} = useState(initState)
+    // const {userState, setUserState} = useState(initState)
+    //console.dir -- will console.log it as an object, so can see its key-value pairs
+    const {userState} = useState(initState)
+
+    function signup(credentials){
+        axios.post("/auth/signup", credentials)
+        .then(res=> console.log(res))
+        .catch(err=>console.log(err.response.data.errMsg))   
+    }
 
     return (
         <UserContext.Provider
          value={{
-            ...userState
+            ...userState,
+            signup
         }}>
             {props.children}
         </UserContext.Provider>
