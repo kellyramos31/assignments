1f5dd915307a676a5b5cc43d92ed5b2a491c137c
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-12-23 18:59:21 -0700

    protected routes added

--- token-auth-exercise/client/src/App.js
+++ token-auth-exercise/client/src/App.js
@@ -2,18 +2,30 @@
 //NOTE:  v. 6 of react-router-dom -- "Redirect" is replaced with "Navigate"
 //Also, the <Route/> tag syntax changed as well; see below for new verions
 
-import React, { useContext } from 'react'
-import { Route, Routes, Navigate } from 'react-router-dom'  
-import NavBar from './components/NavBar.js'
-import Auth from './components/Auth.js'
-import Profile from './components/Profile.js'
-import Public from './components/Public.js'
+//++++++++++++++++++++++++++++++++++++++
+//CURRENT ERROR-- 
+//Error: [ProtectedRoute] is not a <Route> component. All component children of <Routes> must be a <Route> or <React.Fragment>
+//++++++++++++++++++++++++++++++++++++++
+
+//Now closer -- BUT...nothing showing up on dom when sign in -- NavBar shows up, but not the other info...rest is blank
+
+import React, { useContext } from "react"
+import { Route, Routes, Navigate } from "react-router-dom"  
+import NavBar from "./components/NavBar.js"
+import Auth from "./components/Auth.js"
+import Profile from "./components/Profile.js"
+import Public from "./components/Public.js"
+import ProtectedRoute from "./components/ProtectedRoute.js"
 import {UserContext} from "./context/UserProvider.js"
+
+//need useNavigate hook or useParams hook for protected routes instead???
 
 
 export default function App(){
 
   const { token, logout } = useContext(UserContext)
+
+  console.log("token from App.js", token)
 
   return (
     <div className="app">
@@ -23,12 +35,30 @@ export default function App(){
       <Routes>
 
         <Route exact path="/" element={token ? <Navigate to="/profile"/> : <Auth/>}/> 
-     
-        <Route path="/profile" element={<Profile/>}/>
+
+        {/* <Route path="/profile" element={<Profile/>}/>
         
-        <Route path="/public" element={<Public/>}/>
+        <Route path="/public" element={<Public/>}/> */}
      
+
+        <Route element={<ProtectedRoute token={token}/>}>
+            <Route
+                path="/profile" 
+                element={<Profile/>} 
+                navigateTo="/"
+            />
+          
+            <Route
+                path="/public" 
+                element={<Public/>} 
+                navigateTo="/"
+            />
+       </Route>
+   
+   
       </Routes>
     </div>
   )
 }
+
+
