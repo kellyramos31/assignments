d5c6466e6060d13316c8e12994d875e9863fc99b
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2022-01-08 14:14:04 -0700

    working on app.js routes

--- rtv-project/client/src/App.js
+++ rtv-project/client/src/App.js
@@ -1,11 +1,46 @@
+import React, { useContext} from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
+import NavBar from "./components/NavBar.js"
+import Auth from "./components/Auth.js"
+import Profile from "./components/Profile.js"
+import Public from "./components/Public.js"
+import ProtectedRoute from "./components/ProtectedRoute.js"
+import { UserContext } from "./context/UserProvider.js"
 
 
 
 export default function App() {
+
+  const { token, logout } = useContext(UserContext)
+
+  console.log("token from App.js", token)
+
+
   return (
-    <div>
-   
+    <div className="app">
+
+      { token && <NavBar logout={logout} /> }
+
+      <Routes>
+
+        <Route exact path="/" element={token ? <Navigate to="/profile/"/> : <Auth/>}/>
+
+        <Route element={<ProtectedRoute token={token}/>}/>
+          <Route
+            path="/profile"
+            element={<Profile/>}
+            navigateTo="/"
+          />
+
+          <Route
+            path="/public"
+            element={<Public/>}
+            navigateTo="/"
+          />
+      
+
+      </Routes>
+
     </div>
-  );
+  )
 }
