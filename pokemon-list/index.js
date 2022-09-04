0d982b6ea3c76f5557f551f9aac271bb6b80ebcb
N R <ramoskel343@gmail.com>
2022-09-04 09:58:51 -0600

    code to run xmlhttp request in node

--- pokemon-list/index.js
+++ pokemon-list/index.js
@@ -1,30 +1,28 @@
-
-const xhr = new XMLHttpRequest()
+const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://api.vschool.io/pokemon/", true);
 
-xhr.send()
+xhr.send();
 
 xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        const JSONdata = xhr.responseText;
-        const data = JSON.parse(JSONdata);
-        console.log(data.objects[0].pokemon)
-        showOnPage(data.objects[0].pokemon)
-    }
-}
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const JSONdata = xhr.responseText;
+    const data = JSON.parse(JSONdata);
+    console.log(data.objects[0].pokemon);
+    showOnPage(data.objects[0].pokemon);
+  }
+};
 
 //function to show all Pokemon names on web page
 function showOnPage(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const h3 = document.createElement('h3');
-        h3.textContent = arr[i].name;
-        document.body.appendChild(h3);
-    }
+  for (let i = 0; i < arr.length; i++) {
+    const h3 = document.createElement("h3");
+    h3.textContent = arr[i].name;
+    document.body.appendChild(h3);
+  }
 }
 
 //TRY function to show all Pokemon names on web page, but using .map() instead
-
 
 /*
 //EXTRA CREDIT --
@@ -37,3 +35,18 @@ function showOnPage(arr) {
     }
 }
 */
+
+//BELOW--get it to run in node (note: added npm package for xhr2 to enable)
+// const XMLHttpRequest = require("xhr2");
+
+// var xhr = new XMLHttpRequest();
+// // method  // url                     // async?
+// xhr.open("GET", "https://swapi.dev/api/people/1/", true);
+// xhr.send();
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     const JSONdata = xhr.responseText;
+//     const data = JSON.parse(JSONdata);
+//     console.log(data);
+//   }
+// };
